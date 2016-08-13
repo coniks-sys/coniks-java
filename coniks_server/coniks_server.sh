dc4948ea9f50a6980296deac28543f00b009c63a
Marcela Melara <msmelara@gmail.com>
2016-08-13 18:44:23 -0400

    update server and client run scripts

--- coniks_server/coniks_server.sh
+++ coniks_server/coniks_server.sh
@@ -4,42 +4,42 @@
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are 
+#  modification, are permitted provided that the following conditions are
 #  met:
-#  * Redistributions of source code must retain the above copyright 
+#  * Redistributions of source code must retain the above copyright
 #  notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above 
-#  copyright notice, this list of conditions and the following disclaimer 
-#  in the documentation and/or other materials provided with the 
+#  * Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following disclaimer
+#  in the documentation and/or other materials provided with the
 #  distribution.
 #  * Neither the name of Princeton University nor the names of its
 #  contributors may be used to endorse or promote products derived from
 #  this software without specific prior written permission.
 #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-#  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-#  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+#  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+#  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 #  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
 #  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
-#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 #  POSSIBILITY OF SUCH DAMAGE.
 
 ## Runs or stops a CONIKS server instance
 
 # Set all the configs here
-CLASS_DEST="bin" #change this if you built the server somewhere else
-CLASSPATH="-cp $CLASS_DEST"
+CLASS_DEST="target/coniks_server-1.3-SNAPSHOT.jar" #use this for now
+CLASSPATH="-cp ../coniks_common/target/coniks_common-1.3-SNAPSHOT.jar;../crypto/target/coniks-crypto-1.3-SNAPSHOT.jar;$CLASS_DEST"
 SERVER_BIN="org.coniks.coniks_server.ConiksServer"
 CONIKS_SERVERCONFIG="config" #change this if using a different config file
 CONIKS_SERVERLOGS="logs" #change this if storing the logs somewhere else
-RUN_CONIKS="java $CLASSPATH $SERVER_BIN $CONIKS_SERVERCONFIG $CONIKS_SERVERLOGS"
+RUN_CONIKS="java -Xmx512M $CLASSPATH $SERVER_BIN $CONIKS_SERVERCONFIG $CONIKS_SERVERLOGS"
 
 function usage() {
     echo "Usage: $0 <start | test | stop | clean>"
@@ -100,7 +100,7 @@ elif [ "$CMD" = "stop" ]; then
     fi
 
      echo "Stopping the CONIKS server."
-     
+
      kill `cat $CONIKS_SERVERLOGS/pid` >/dev/null
      rm -f $CONIKS_SERVERLOGS/pid
 
@@ -112,7 +112,7 @@ elif [ "$CMD" = "clean" ]; then
     # write to one of the logs
      if [ `ps ax | grep -c $SERVER_BIN` -gt 1 ]; then
          echo "Stopping the CONIKS server."
-     
+
          kill `cat $CONIKS_SERVERLOGS/pid` >/dev/null
          rm -f $CONIKS_SERVERLOGS/pid
     fi
